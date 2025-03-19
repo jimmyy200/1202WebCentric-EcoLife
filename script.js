0d2db4849963d831eae33de464e9164f28efd854
jimmyy200 <jimmy322@my.yorku.ca>
2025-03-19 14:27:13 -0400

    basic javascript for tracker

---
+++ script.js
@@ -0,0 +1,34 @@
+function calculate(event){
+    var total = 0;
+    var meatV = meat.value;
+    var transV = transport.value;
+    const beefV = 99.48;
+    const porkV = 12.31;
+    const chicV = 9.87;
+    const carV = .150;
+    const busV = .050;
+    const trainV = .035;
+    const walkV = 0;
+
+    if (meatV == "beef"){
+        total += 60*beefV;
+    } else if (meatV == "pork"){
+        total += 60*porkV;
+    } else if (meatV == "chicken"){
+        total += 60*chicV;
+    }
+
+    if (transV == "car"){
+        total += 10000 * carV;
+    } else if (transV == "bus"){
+        total += 10000 * busV;
+    } else if (transV == "train"){
+        total += 10000 * trainV;
+    } else {
+        total += 0;
+    }
+
+    result.innerHTML = "You approximately emit " + total + "kg of CO2 per year";
+    result.innerHTML += `<br>`;
+    result.innerHTML += "The average person emits 4500kg of CO2 per year"
+};
